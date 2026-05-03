6b8234f0ffb27bd41d27eb3afae7be5dc10f5a4d
Ignacy Pękała <ignacy@pekala.eu>
2026-05-03 20:47:32 +0200

    Sometimes works

--- arithmetic_sequence.asm
+++ arithmetic_sequence.asm
@@ -2,91 +2,140 @@ global arithmetic_sequence
 
 arithmetic_sequence:
     ; Parameters:
-    ; rdi - *a_0 (unsigned)
-    ; rsi - *a_1 (unsigned)
-    ; rdx - *a_k (unsigned) (no important input)
+    ; rdi - *a_0 (2s complement)
+    ; rsi - *a_1 (2s complement)
+    ; rdx - *a_k (no important input)
     ; rcx - n (unsigned)
     ; r8 - k (signed)
+    ; Registers:
+    ; r10/rcx - iteration
+    ; r9, r11, r12, rax, rdx - math
+    
+    push r12
 
     ; The difference (a_1 - a_0) can cause an overflow, requiring an extra
     ; (n+1)-th limb. We compute this virtual limb by sign-extending the highest
     ; limbs of A0 and A1 and subtracting them (with borrow) to prepend the 
-    ; correct sign mask (0x0000000000000000 or 0xffffffffffffffff) before multiplying.
+    ; correct sign mask (0x0000000000000000 or 0xffffffffffffffff) 
+    ; before multiplying.
+
+    ; r11, r9 - masks for the virtual limb of a_0 and a_1 respectively
     mov r11, [rdi + rcx * 8 - 8]
     mov r9, [rsi + rcx * 8 - 8]
     sar r11, 63
     sar r9, 63
 
+    ; r10 - limb index for the .compute_common_difference loop (i)
     ; Zeroing r10 for iteration clears the CF flag, which prevents sbb from
     ; including any borrow in the first iteration.
     xor r10, r10
 
-
+; Performs subtraction (a_1 - a_0) limb by limb. The output is written into a_k.
 ; while (rcx > 0) { ...; i++; rcx--; }
 .compute_common_difference:
     ; a_k[i] <-- a_1[i] - a_0[i]
+    ; rax - used for subtraction
     mov rax, [rsi + r10 * 8]
-    sbb rax,[rdi + r10 * 8]
+    sbb rax, [rdi + r10 * 8]
     mov [rdx + r10 * 8], rax
     ; This approach eliminates the need for cmp which would overwrite CF.
     inc r10
     dec rcx
     jnz .compute_common_difference
 
-    ; Subtract the sign-extensions with borrow from the last iteration.
-    sbb r9, r11
+    ; r9 now holds the most-significant limb of a_k.
+    sbb r9, r11 
 
-    ; r10 == n, rcx == 0, a_k = a_1 - a_0
+    ; r10 == n, rcx == 0, {r9, ...r_k} == (a_1 - a_0)
 
-    ; [REDACT] Now we will turn a_k into its absolute value so that multiplying becomes easier.
-    ; Create a mask for negating.
-    mov r11, [rdx + r10 * 8 - 8]
-    sar r11, 63
+    ; [REDACT] We will now turn a_k into its absolute value so that multiplying becomes easier.
+    ; r12 - a mask for negating a_k
+    mov r12, [rdx + r10 * 8 - 8]
+    sar r12, 63
     
+; Applies the mask to the n-1 least significant limbs of a_k to extract the absolute value.
 ; for (int i = 0; i < n; i++) {a_k[i] <-- |a_k[i]|}
 .turn_ak_absolute:
-    xor [rdx + r10 * 8 - 8], r11
+    xor [rdx + rcx * 8], r12
     inc rcx
     dec r10
     jnz .turn_ak_absolute
 
-    ; rcx == n, r10 == 0 
-
     ; Add 1 if a_k was negative
-    neg r11
-    add [rdx + rcx * 8 - 8], r11
+    neg r12
+    add [rdx + rcx * 8 - 8], r12
 
+    ; rcx == n, r10 == 0, {r9, ...r_k} == |a_1 - a_0|
 
-    ; Now that a_k = |a_1 - a_0| we can do (n - 1) * |a_1 - a_0|
+    sub r8, 1
 
-    sub rcx, 1
-
-    ; a_0 is no longer needed but rdx will be used by imul.
+    ; rdi - holds the pointer to a_k (a_0 is discarded)
+    ; r11 - holds the carry for multiplication
     mov rdi, rdx
     xor r11, r11
 
-; for ( i = 0; i < n - 1; i++) {...}
+; Calculate |a_1 - a_0| * (k - 1) for the n - 1 least significant limbs
+; for ( i = 0; i < n; i++) {...}
 .multiply_common_difference_absolute:
-    mov rax,[rdi + r10 * 8]
-    mul rcx
+    mov rax, [rdi + r10 * 8]
+    mul r8
     ; Add previous limb's carry to the lower half and absorb 
     ; any overflow into the upper half.
     add rax, r11
     adc rdx, 0
     mov [rdi + r10 * 8], rax
     mov r11, rdx
-    
+
     inc r10
-    cmp r10, rcx
-    jl .multiply_common_difference_absolute
+    dec rcx
+    jnz .multiply_common_difference_absolute
 
-    mov rax,[rdi + rcx * 8]
-    imul r10
+    ; Add the final carry absorbing its potential overflow into rdx
+    xor rdx, rdx
+    add r9, r11
+    adc rdx, 0
 
-    mov rdx, 0
+; Reapply the sign    
+; r12 equals 1 if a_1 - a_0 was negative or 0 othwerise
+; rdi - holds the pointer to a_k
+; r10 == n, rcx == 0, {r9, ...r_k} = (k - 1) * |a_1 - a_0|
+.revert_absolute:
+    xor [rdi + rcx * 8], r12
+    inc rcx
+    dec r10
+    jnz .revert_absolute
+    
+    neg r12
+    add [rdi + rcx * 8 - 8], r12
+    
+
+    ; Clear the flags before addition
+    xor rdx, rdx
+
+; Finally add a_1
+; rdi - holds the pointer to a_k
+; rsi - holds the pointer to a_1
+; rax - used during addition
+; r10 == 0, rcx == n, {r9, ...r_k} = (k - 1) * |a_1 - a_0|
+.add_a1_to_result:
+    mov rax, [rdi + r10 * 8]
+    adc rax, [rsi + r10 * 8]
+    mov [rdi + r10 * 8], rax
+    inc r10
+    dec rcx
+    jnz .add_a1_to_result
+
+    adc r9, 0
+
+    
     ; mov rax, 0
     ; Return values
     ; rax - lo
     ; rdx - hi
-    ; [rdx] - a_k
+    ; [rdi] - a_k
+    mov rdx, 0
+    mov rax, r9 
+
+    pop r12
+
     ret
