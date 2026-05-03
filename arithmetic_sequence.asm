ab0296526107e2d0927d565676927251fb81c6b2
Ignacy Pękała <ignacy@pekala.eu>
2026-05-03 11:08:25 +0200

    Correct subtraction I think

--- arithmetic_sequence.asm
+++ arithmetic_sequence.asm
@@ -1,21 +1,35 @@
 global arithmetic_sequence
 
+; 
+.compute_step:
+    
+        
+
 arithmetic_sequence:
     ; Parameters:
-    ; rdi - a_0
-    ; rsi - a_1
-    ; rdx - a_k 
+    ; rdi - *a_0
+    ; rsi - *a_1
+    ; rdx - *a_k 
     ; rcx - n
     ; r8 - k
 
-    ; Calculate the arithmetic sequence difference
-    mov r9, [rsi]
-    sub r9, [rdi]
+    ; Zeroing r10 for iteration clearing the CF flag, which causes sbb to act
+    ; as sub on first iteration.
+    xor r10, r10
 
-    sub rcx, 1
+.compute_common_difference:
+    ; a_k[i] <-- a_1[i] - a_0[i]
+    mov r9, [rsi + r10 * 8]
+    sbb r9,[rdi + r10 * 8]
+    mov [rdx + r10 * 8], r9
 
-    mov [rdx], r9
-    mov rdx, rcx
+    ; Double iterators eliminate the need for cmp, which would 
+    ; overwrite the CF flag.
+    inc r10
+    dec rcx
+    jnz .compute_common_difference
+
+    mov rdx, 0
     mov rax, 0
     ; Return values
     ; rax - lo
