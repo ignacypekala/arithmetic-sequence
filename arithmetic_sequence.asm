0090c50dd9b8cea4ee9f63e58cbb58f985bedcc5
Ignacy Pękała <ignacy@pekala.eu>
2026-05-03 14:48:56 +0200

    Working subtraction with overflow handling

--- arithmetic_sequence.asm
+++ arithmetic_sequence.asm
@@ -1,20 +1,25 @@
 global arithmetic_sequence
 
-; 
-.compute_step:
-    
-        
-
 arithmetic_sequence:
     ; Parameters:
-    ; rdi - *a_0
-    ; rsi - *a_1
-    ; rdx - *a_k 
-    ; rcx - n
-    ; r8 - k
+    ; rdi - *a_0 (unsigned)
+    ; rsi - *a_1 (unsigned)
+    ; rdx - *a_k (unsigned) (no important input)
+    ; rcx - n (unsigned)
+    ; r8 - k (signed)
 
-    ; Zeroing r10 for iteration clears the CF flag, which causes sbb to act
-    ; as sub on the first iteration.
+    ; A0 and A1 are signed integers (two's complement). 
+    ; Their difference can cause a signed overflow, requiring an extra (n+1)-th limb.
+    ; We compute this virtual limb by sign-extending the highest limbs of A0 and A1
+    ; and subtracting them (with borrow) to prepend the correct sign mask 
+    ; (0x0000000000000000 or 0xffffffffffffffff) before multiplying.
+    mov r11, [rdi + rcx * 8 - 8]
+    mov rax, [rsi + rcx * 8 - 8]
+    sar r11, 63
+    sar rax, 63
+
+    ; Zeroing r10 for iteration clears the CF flag, which prevents sbb from
+    ; including any borrow in the first iteration.
     xor r10, r10
 
 ; while (rcx > 0) { ...; i++; rcx--; }
@@ -23,14 +28,17 @@ arithmetic_sequence:
     mov r9, [rsi + r10 * 8]
     sbb r9,[rdi + r10 * 8]
     mov [rdx + r10 * 8], r9
-    ; Double iterators eliminate the need for cmp, which would 
+    ; Doubled iterators eliminate the need for cmp, which would 
     ; overwrite the CF flag.
     inc r10
     dec rcx
     jnz .compute_common_difference
 
+    ; Subtract the sign-extensions with borrow from the last iteration
+    sbb rax, r11
+
     mov rdx, 0
-    mov rax, 0
+    ; mov rax, 0
     ; Return values
     ; rax - lo
     ; rdx - hi
