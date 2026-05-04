c6b2ed9bf58cfa1a45cb5ea16eb8b55bd73d0577
Ignacy Pękała <ignacy@pekala.eu>
2026-05-04 18:17:38 +0200

    Working unsigned multiplication with quirks

--- arithmetic_sequence.asm
+++ arithmetic_sequence.asm
@@ -9,9 +9,11 @@ arithmetic_sequence:
     ; r8 - k (signed)
     ; Registers:
     ; r10/rcx - iteration
-    ; r9, r11, r12, rax, rdx - math
+    ; r9, r11, r12, r13, rax, rdx - math
     
     push r12
+    push r13
+    push r14
 
     ; The difference (a_1 - a_0) can cause an overflow, requiring an extra
     ; (n+1)-th limb. We compute this virtual limb by sign-extending the highest
@@ -47,13 +49,22 @@ arithmetic_sequence:
     ; r9 now holds the most-significant limb of a_k.
     sbb r9, r11 
 
-
     ; Using unsigned multiplication to multiply (a_1 - a_0)*(k - 1).
     ; Because we're interpeting signed integers as unsigned there is a potential
     ; error but it will be nullified by further subtraction.
 
     dec r8 ; r8 == (k - 1)
-    mov rdi, rax ; Discarding a_0 to hold *a_k
+    mov rdi, rdx ; Discarding a_0 to hold *a_k
+
+    ; Create an error nullifier based on *a_k's sign
+    mov r12, [rdi + r10 * 8 - 8]
+    sar r12, 63
+    ; r12 now contains 1 if a_k was negative and 0 otherwise
+
+    ; Create a mask based on the multiplier
+    mov r13, r8
+    sar r13, 63
+
     ; Clears the flags and r11 for multiplication
     xor r11, r11 
     ; rax and rdx will be used during multiplication
@@ -62,13 +73,23 @@ arithmetic_sequence:
     ; r11 will hold the carry between multiplications
 
 .multiply_common_difference_by_index:
+    ; Perform unsigned multplication of (a_k[i] * (k - 1)) while preserving the
+    ; carry for a_k[i + 1].
     mov rax, [rdi + rcx * 8]
+    mov r14, rax ; Save a_k[i] to later subtract from rdx
     mul r8
-    add rax, r11
+    ; Absorb the previous carry
+    add rax, r11 
     ; Include the carry from the addition to the current multiplication carry.
-    adc rdx, 0 
+    adc rdx, 0
     mov [rdi + rcx * 8], rax
+    
+    ; Subtract a_k[i] from the carry if the multiplier was negative
+    and r14, r13
+    sub rdx, r14
+    ; Pass the carry for the next iteration
     mov r11, rdx
+
     inc rcx
     dec r10
     jnz .multiply_common_difference_by_index
@@ -82,34 +103,40 @@ arithmetic_sequence:
 
     ; Now (a_1 - a_0)*(k - 1) is stored across {rdx, rax, ...a_k}, provided
     ; that both (a_1 - a_0) and (k - 1) are positive.
-    ; TODO Subtract the errors
+
+    ; If (a_1 - a_0) was negative we subtract 2^(64*n) from a_k by decrementing
+    ; the n+1th limb and include the borrow in rdx.
+    and r8, r12
+    sub rax, r8
+    sbb rdx, 0
 
     ; Clear the flags before addition
-    xor rdx, rdx
+    xor r8, r8
 
 ; Finally add a_1
 ; rdi - holds the pointer to a_k
 ; rsi - holds the pointer to a_1
-; rax - used during addition
-; r10 == 0, rcx == n, {r9, ...r_k} = (k - 1) * |a_1 - a_0|
+; r8 - used during addition
+; r10 == 0, rcx == n, {rdx, rax, ...a_k} = (k - 1) * |a_1 - a_0|
 .add_a1_to_result:
-    mov rax, [rdi + r10 * 8]
-    adc rax, [rsi + r10 * 8]
-    mov [rdi + r10 * 8], rax
+    mov r8, [rdi + r10 * 8]
+    adc r8, [rsi + r10 * 8]
+    mov [rdi + r10 * 8], r8
     inc r10
     dec rcx
     jnz .add_a1_to_result
 
-    adc r9, 0
+    adc rax, 0
+    adc rdx, 0
     
     ; mov rax, 0
     ; Return values
     ; rax - lo
     ; rdx - hi
     ; [rdi] - a_k
-    mov rdx, 0
-    mov rax, r9 
 
+    pop r14
+    pop r13
     pop r12
 
     ret
